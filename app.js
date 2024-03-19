0174cbacb1235c3a648ade671c464eaec02905cc
spayili <sharath.ln253@gmail.com>
2024-03-19 01:24:54 -0700

    Changes to be committed: 	new file:   .env 	new file:   .gitignore 	new file:   app.js 	new file:   authentication/UserContext.js 	new file:   authentication/userDB/users.json 	new file:   controller/usersController.js 	new file:   data/data/users.json 	new file:   model/user.js 	new file:   package-lock.json 	new file:   package.json 	new file:   routes/usersRoute.js 	new file:   tests/users.rest

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// Add this linebefore any environment variables are used
+const dotenv = require('dotenv'); 
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/usersRoute');
+dotenv.config(); // Add this line for the environment variables to be used
+// Create express app
+const app = express();
+//if the environment variable PORT is not defined, the server will run on port 3000
+// this is stored and defied in the config.env file
+const PORT = process.env.PORT || 3000; 
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
